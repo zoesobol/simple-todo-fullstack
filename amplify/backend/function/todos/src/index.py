3a62f72265770a511ae1ea92e936877e8a3ff7e8
Zoe Sobol <zoesobol98@gmail.com>
2022-01-22 15:01:50 -0300

    slightly functional todo shower

--- amplify/backend/function/todos/src/index.py
+++ amplify/backend/function/todos/src/index.py
@@ -1,18 +1,36 @@
 import json
 import awsgi
+import boto3
+import os
+
 from flask_cors import CORS
 from flask import Flask, jsonify, request
+from uuid import uuid4
 
 app = Flask(__name__)
 CORS(app)
+
+client = boto3.client("dynamodb")
+TABLE = os.environ.get("STORAGE_SIMPLETODOSTORAGE_NAME")
 
 # Constant variable with path prefix
 BASE_ROUTE = "/simpletodoapi"
 
 
+@app.route(BASE_ROUTE, methods=['POST'])
+def create_todo():
+    request_json = request.get_json()
+    client.put_item(TableName=TABLE, Item={
+        'id': {'S': str(uuid4())},
+        'name': {'S': request_json.get("name")},
+        'description': {'S': request_json.get("description")},
+    })
+    return jsonify(message="item created")
+
+
 @app.route(BASE_ROUTE, methods=['GET'])
 def list_todos():
-    return jsonify(message="hello world from list_todos()")
+    return jsonify(data=client.scan(TableName=TABLE))
 
 
 def handler(event, context):
--- src/App.js
+++ src/App.js
@@ -5,25 +5,42 @@ import { API } from "aws-amplify";
 
 function App() {
   const [message, setMessage] = useState(null);
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const listTodos = todos.map((d) => (
+    <div>
+      <h2>{d.name.S}</h2>
+      <p>{d.description.S}</p>
+    </div>
+  ));
 
   const getData = async () => {
     const data = await API.get("simpletodoapi", "/simpletodoapi");
-    setMessage(data.message);
-    console.log(data);
+    setTodos(data.data.Items);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{message}</h1>
-      </header>
-    </div>
-  );
+  console.log(todos);
+  if (isLoading) {
+    return (
+      <div className="App">
+        <header className="App-header">{listTodos}</header>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>To-do's</h1>
+          {listTodos}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
