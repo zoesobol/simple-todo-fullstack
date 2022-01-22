e67c919d42e09d4bbdb259d312e7b1104c1bbc1e
Zoe Sobol <zoesobol98@gmail.com>
2022-01-22 07:46:16 -0300

    pushed to the cloud

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "simpletodoapi": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "simpletodoapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
